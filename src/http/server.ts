bbc7c354193cffcf66cdd5a9dcb889e6db4efaaf
Olliveer <j.oliveira90@live.com>
2024-02-11 19:44:20 -0300

    refactor: server.ts and add prisma.ts

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,22 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { getLicenses } from "./routes/get-licenses";
+import { createLicense } from "./routes/create-license";
+import { getSoftwares } from "./routes/get-softwares";
 
+const app = Fastify({ logger: true });
+await app.register(cors, {
+  origin: "*",
+});
 
+app.register(getLicenses);
+app.register(createLicense);
+app.register(getSoftwares);
 
-
-const app = Fastify({logger: true })
-
-
-
-
-
-
-app.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+app.listen({ port: 3001, host: "0.0.0.0" }, function (err, address) {
   if (err) {
-    app.log.error(err)
-    process.exit(1)
+    app.log.error(err);
+    process.exit(1);
   }
-  app.log.info(`server listening on ${address}`)
-})
+  app.log.info(`server listening on ${address}`);
+});
